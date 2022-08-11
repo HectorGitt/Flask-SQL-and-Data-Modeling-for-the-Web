eedf7294f20ba20857ddd1427f6ebb5f8bf54f84
HectorGitt <adeniyi.olaitanhector@yahoo.com>
2022-08-11 15:56:08 +0100

    seperated models.py into file

---
+++ models.py
@@ -0,0 +1,90 @@
+from app import *
+from flask_sqlalchemy import SQLAlchemy
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+db = SQLAlchemy()
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.relationship('VenueGenre', backref='venue',lazy=True)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean())
+    seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='venue',lazy=True)
+    
+    def __repr__(self):
+        return f'{self.name}'
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.relationship('ArtistGenre', backref='artist',lazy=True)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean())
+    seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='artist',lazy=True)
+    def __repr__(self):
+        return f'{self.name}'
+
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'Show'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    start_time = db.Column(db.DateTime(), nullable=False)
+    
+    def __repr__(self):
+      return f'{self.artist_id} {self.venue_id}'
+    
+    
+class Genre(db.Model):
+    __tablename__ = 'Genre'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    #model relationships
+    artist_genre = db.relationship('ArtistGenre', backref='genre', lazy=True)
+    venue_genre = db.relationship('VenueGenre', backref='genre', lazy=True)
+    
+    def __repr__(self):
+        return f'{self.name}'
+    
+class ArtistGenre(db.Model):
+    __tablename__ = 'artist_genre'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    genre_id = db.Column(db.Integer, db.ForeignKey('Genre.id'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    
+    def __repr__(self):
+      return f'{Genre.query.filter_by(id=self.genre_id).first().name}'
+    
+class VenueGenre(db.Model):
+    __tablename__ = 'venue_genre'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    genre_id = db.Column(db.Integer, db.ForeignKey('Genre.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+
+    def __repr__(self):
+        return f'{Genre.query.filter_by(id=self.genre_id).first().name}'
